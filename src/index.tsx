34a5b25c0069d1c066509824f15aee84adee287a
seanccc <sean831004@gmail.com>
2018-06-21 16:12:59 +0800

    scss settings done

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import './index.scss'
 
 ReactDOM.render(
-    <div>Hello, world10!</div>,
+    <div className='test'>Hello, world10!</div>,
     document.getElementById("app")
 );
