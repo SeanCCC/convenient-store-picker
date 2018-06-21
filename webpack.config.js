4f73a96c805f89275cc4b5a21196d6752e688d64
seanccc <sean831004@gmail.com>
2018-06-21 15:53:52 +0800

    HMR,devserver,webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+const  HTMLWebpackPluginConfig = new HtmlwebpackPlugin({
+  title: 'testApp',
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html'
+})
+
+module.exports = {
+  // entry: './src/index.tsx',
+  entry: [
+    './src/index.tsx',
+    // 'webpack-hot-middleware/client?http://localhost:3000'// host 與 port
+  ],
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+  }
+};
